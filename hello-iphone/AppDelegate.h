bbbb670a4d1db074ca0ca860ec7e3201ff6c8b68
student <student@H702-20.local>
2013-07-03 14:15:42 -0400

    Initial Commit

---
+++ hello-iphone/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  hello-iphone
+//
+//  Created by student on 7/3/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ hello-iphone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hello-iphone
+//
+//  Created by student on 7/3/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
